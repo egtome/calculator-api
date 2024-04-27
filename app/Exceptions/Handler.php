89b815f4b59ac83e2a3ca0f86b5a229512de5147
Gino Tome <gino@pixouls.com>
2024-04-27 15:28:35 -0300

    Return json in case of not found

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,8 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\JsonResponse;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -47,4 +49,13 @@ class Handler extends ExceptionHandler
             //
         });
     }
+
+    public function render($request, Throwable $exception)
+    {
+        if ($exception instanceof NotFoundHttpException) {
+            return response()->json(['error' => 'Not Found'], 404);
+        }
+    
+        return parent::render($request, $exception);
+    }   
 }
